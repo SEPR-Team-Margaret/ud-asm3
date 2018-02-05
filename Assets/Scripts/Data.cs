3c9b822fb0fe859d9938479b692ff77b87d64311
David Clarke <dclarkeu@gmail.com>
2018-02-05 22:20:49 +0000

    Added Data.cs for global data passing

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Data
+{
+
+    private static bool isDemo;
+
+    public static bool IsDemo
+    {
+        get
+        {
+            return isDemo;
+        }
+        set
+        {
+            isDemo = value;
+        }
+    }
+}
