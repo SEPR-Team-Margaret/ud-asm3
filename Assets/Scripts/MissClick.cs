213ce3730cf82bced252f43895effd98916c77fb
ollie1067 <33376274+ollie1067@users.noreply.github.com>
2018-02-16 14:58:52 +0000

    Minigame working
    Minigame scene shows fully functional minigame.
    I have copy pasted it over to main mappit scene, however will need to work to run smoothly.

---
+++ Assets/Scripts/MissClick.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissClick : MonoBehaviour
+{
+    public static int missedPVC = 0;
+    public int testresult = 1;
+    public GameObject testforstartmenu;
+    public GameObject missedclick;
+    public GameObject buffawarded;
+
+    // Use this for initialization
+    void Start()
+    {
+        missedclick = GameObject.Find("BuffNotAwarded!");
+        missedclick.gameObject.SetActive(false);
+    }
+    
+    void OnMouseDown()
+    {
+        testforstartmenu = GameObject.Find("StartButton");
+        if (testforstartmenu == true)
+        {
+            return;
+        }
+        else
+        {
+            testresult = 1;
+        }
+
+        buffawarded = GameObject.Find("BuffAwarded");
+        if (buffawarded == true)
+        {
+            return;
+        }
+        if (testresult == 1) 
+        {
+            gameObject.SetActive(false);
+            missedclick.gameObject.SetActive(true);
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
---
+++ Assets/Scripts/PVC.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PVC : MonoBehaviour
+{
+    public static int caughtPVC = 0;
+    public Vector3 vector;
+    public GameObject BuffAwarded;
+    // Use this for initialization
+    void Start()
+    {
+        BuffAwarded = GameObject.Find("BuffAwarded");
+        vector = new Vector3(13, 17, 0);
+        BuffAwarded.gameObject.SetActive(false);
+        gameObject.SetActive(false);
+    }
+
+    public void OnCollisionEnter(Collision collision)           //For each different side of the border there is a seperate if function, as each side has a different reflection normal
+    {                                                           //There is a different gameobject referenced for each plane of border i.e."Border_Right"
+        if (collision.gameObject.name == "Border_Right")        //If the sprite collides with the right side of the minigame border, reflect with respect to the right normal (Vector3.right)
+        {
+            vector = Vector3.Reflect(vector, Vector3.right);
+        }
+
+        if (collision.gameObject.name == "Border_Left")         //If the sprite collides with the right side of the minigame border, reflect with respect to the left normal (Vector3.left)
+        {
+            vector = Vector3.Reflect(vector, Vector3.left);
+        }
+
+        if (collision.gameObject.name == "Border_Top")          //If the sprite collides with the right side of the minigame border, reflect with respect to right normal (Vector3.top)
+        {
+            vector = Vector3.Reflect(vector, Vector3.up);
+        }
+
+        if (collision.gameObject.name == "Border_Bottom")       //If the sprite collides with the right side of the minigame border, reflect with respect to right normal (Vector3.bottom)
+        {
+            vector = Vector3.Reflect(vector, Vector3.down);
+        }
+
+
+    }
+
+    public void OnMouseDown()
+    {
+        gameObject.SetActive(false);
+        BuffAwarded.gameObject.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(vector);
+    }
+}
